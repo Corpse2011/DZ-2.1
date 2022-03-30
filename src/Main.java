c978d8930f9e2274f81a3982930e5f70822f85c2
Corpse2011 <boroda_gk@bk.ru>
2022-03-30 18:58:56 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и параметров программы:
+        int ticketPrice = 100; // стоимость одного билета
+        int bonusMiles = 10; // 10р = 1 миля
+        // Рассчитываете количество бонусных милль, используя
+        System.out.println("Количество бонусных миль:"+(ticketPrice / bonusMiles));
+    }
+}
